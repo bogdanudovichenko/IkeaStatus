4903ebc0eb1f170f332b63e87c715f4b87da4ac5
Bogdan Udovichenko <bogdanudovichenkodev@gmail.com>
2022-01-07 17:42:58 +0200

    Move browser to headless mod

--- Extensions/BrowserExtensions.cs
+++ Extensions/BrowserExtensions.cs
@@ -6,7 +6,7 @@ public static class BrowserExtensions
     {
         var options = new BrowserTypeLaunchOptions
         {
-            Headless = false
+            Headless = true
         };
 
         try
